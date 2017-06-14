a362a233ed6ec0892dc060557baca9c91b6ab968
musa.zulu <zuluchs@gmail.com>
2017-06-14 11:56:59 +0200

    code clean-up

--- source/Web_API_Demo.Tests.Common/Domain/ProductBuilder.cs
+++ source/Web_API_Demo.Tests.Common/Domain/ProductBuilder.cs
@@ -1,14 +1,9 @@
 ﻿using PeanutButter.RandomGenerators;
-using System;
 using Web_API_Demo.Core.Domain;
 
 namespace Web_API_Demo.Tests.Common.Domain
 {
     public class ProductBuilder : GenericBuilder<ProductBuilder, Product>
-    {
-        public ProductBuilder WithId(Guid id)
-        {
-            return WithProp(p => p.Id = id);
-        }
+    {        
     }
 }
